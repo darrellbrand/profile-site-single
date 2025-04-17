2acebcf0ed1e5c222b4bd66a33b242bc7c07adcf
dj <darrellbrand@gmail.com>
2025-04-17 03:27:15 -0700

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
       <AppNavBar></AppNavBar>
 
       <main className="w-full flex flex-col items-center px-4 sm:px-6 md:px-8 max-w-7xl mx-auto">
-        <h1>fuck</h1>
         <HeroSectionOne />
         <AndroidSectionOne />
         <ProfileSectionOne />
