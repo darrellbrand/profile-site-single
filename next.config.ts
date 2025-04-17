0cbea0a529a3d0f49caf3e9c774d91b798a4c58f
dj <darrellbrand@gmail.com>
2025-04-17 15:18:39 -0700

    layout

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+
+const repoName = 'profile-site-static';
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  trailingSlash: true, // ensures directories end in `/` for static
 };
+
 
 export default nextConfig;
