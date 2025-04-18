8025ac86af0fb22a1065b37127b58308df349273
dj <darrellbrand@gmail.com>
2025-04-17 17:38:55 -0700

    layout

--- src/components/profile-section.tsx
+++ src/components/profile-section.tsx
@@ -18,7 +18,7 @@ export default function ProfileSectionOne() {
                         <h1 className=" my-10 text-center text-4xl font-bold dark:text-neutral-100 md:text-4xl lg:text-7xl text-slate-500 self-start mx-5">About</h1>
                         <Card className=" w-full items-center px-5 pt-10  ">
                             <CardHeader className="w-full text-2xl ">
-                                <CardTitle className="pb-5 whitespace-normal sm:whitespace-nowrap sm:overflow-hidden sm:text-ellipsis ">Darrell Friday Software Engineer.</CardTitle>
+                                <CardTitle className="pb-5 text-center whitespace-normal sm:whitespace-nowrap sm:overflow-hidden sm:text-ellipsis text-3xl  font-bold dark:text-neutral-100 md:text-4xl lg:text-6xl text-slate-500 ">Darrell Friday - Software Engineer</CardTitle>
                             </CardHeader>
                             <CardContent className="flex flex-col  sm:flex-col md:flex-col gap-10 justify-between items-center">
                                 <img
@@ -26,7 +26,7 @@ export default function ProfileSectionOne() {
                                     alt="Landing page preview"
                                     className=" max-w-xs w-full h-full object-cover rounded-full"
                                 />
-                                <CardDescription className="text-xl  max-w-4xl">
+                                <CardDescription className="text-xl text-center max-w-4xl md:text-xl lg:text-2xl">
                                     Experienced in building Android and full-stack web applications with a strong focus on performance and maintainability. At ESChat, I worked on Android apps using Kotlin, Java, Jetpack Compose, MVVM, and Dagger Hilt, along with writing automation tests using Appium to ensure quality.
                                     I also develop web frontends with Next.js and connect them to secure, scalable Spring Boot backends. Comfortable working across multiple languages including Python, Lua, Bash, C/C++, and NSIS, I bring a solid engineering foundation and a detail-oriented mindset to every project.</CardDescription>
                             </CardContent>
