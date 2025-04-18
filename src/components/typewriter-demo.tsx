0747a0fb6bc074536a82301c71b4523d3dbf87a2
dj <darrellbrand@gmail.com>
2025-04-17 19:45:51 -0700

    layout

--- src/components/typewriter-demo.tsx
+++ src/components/typewriter-demo.tsx
@@ -2,29 +2,23 @@
 import { TypewriterEffect } from "./ui/typewriter-effect";
 
 export function TypewriterEffectDemo() {
-  const words = [
-    {
-      text: "Build",
-    },
-    {
-      text: "awesome",
-    },
-    {
-      text: "apps",
-       className: "text-android dark:text-blue-500"
-    },
-    {
-      text: "with",
-    },
-    {
-      text: "Darrell.",
-      className: "text-blue-500 dark:text-blue-500",
-    },
-  ];
-  return (
-    <div>
-      <TypewriterEffect words={words} />
-     
-    </div>
-  );
+    const words = [
+        {
+            text: "Email",
+        },
+        {
+            text: "me",
+            className: "text-android"
+        },
+        {
+            text: "today!",
+        },
+       
+    ];
+    return (
+        <div>
+            <TypewriterEffect words={words} />
+
+        </div>
+    );
 }
