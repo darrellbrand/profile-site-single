e86bbb46b0949ef0504655fd0ba60f72db32075c
dj <darrellbrand@gmail.com>
2025-04-20 19:33:02 -0700

    add video links

--- src/components/email-form.tsx
+++ src/components/email-form.tsx
@@ -7,7 +7,7 @@ import { TypewriterEffectDemo } from './typewriter-demo'
 export default function EmailForm() {
     return (
         <section id="contact" className='w-full  bg-neutral-400/60 dark:bg-neutral-800 ' >
-            <div className=' flex flex-col w-full max-w-7xl mx-auto'>
+            <div className=' flex flex-col w-full max-w-7xl mx-auto mb-50'>
                 <h1 className='my-20 w-full text-2xl font-bold dark:text-neutral-200 md:text-3xl lg:text-5xl text-neutral-600'>Contact</h1>
                 <div className='pt-20 flex flex-col items-center mx-auto max-w-7xl border rounded-2xl w-full  bg-neutral-300/80 dark:bg-neutral-700'>
                         <TypewriterEffectDemo></TypewriterEffectDemo>
