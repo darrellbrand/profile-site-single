f99354f817ec333d4de83c8e4378ced59b895d47
dj <darrellbrand@gmail.com>
2025-04-21 00:39:21 -0700

    layout

--- src/components/android-section.tsx
+++ src/components/android-section.tsx
@@ -25,7 +25,7 @@ export default function AndroidSectionOne() {
                                     </img>
                                     <a href="https://www.youtube.com/watch?v=R8hJNKSZNo0&t=29s" target="_blank" rel="noopener noreferrer">
                                         <button className=" transform rounded-2xl  text-neutral-300 bg-neutral-700   px-6 py-2  font-medium  transition-all duration-300 hover:-translate-y-0.5 hover:bg-neutral-400 dark:bg-neutral-600 dark:text-neutral-300 dark:hover:bg-neutral-500">
-                                            Demo Video
+                                           Watch Demo Video
                                         </button>
                                     </a>
 
@@ -53,7 +53,7 @@ export default function AndroidSectionOne() {
                                     </img>
                                     <a href="https://www.youtube.com/shorts/pOssuVUjEiw" target="_blank" rel="noopener noreferrer">
                                         <button className=" transform rounded-2xl  text-neutral-300 bg-neutral-700   px-6 py-2  font-medium  transition-all duration-300 hover:-translate-y-0.5 hover:bg-neutral-400 dark:bg-neutral-600 dark:text-neutral-300 dark:hover:bg-neutral-500">
-                                            Demo Video
+                                           Watch Demo Video
                                         </button>
                                     </a>
 
@@ -82,7 +82,7 @@ export default function AndroidSectionOne() {
                                     </img>
                                     <a href="https://youtube.com/shorts/wFMom2i1Nzc?feature=share" target="_blank" rel="noopener noreferrer">
                                         <button className=" transform rounded-2xl  text-neutral-300 bg-neutral-700   px-6 py-2  font-medium  transition-all duration-300 hover:-translate-y-0.5 hover:bg-neutral-400 dark:bg-neutral-600 dark:text-neutral-300 dark:hover:bg-neutral-500">
-                                            Demo Video
+                                            Watch Demo Video
                                         </button>
                                     </a>
 
@@ -113,7 +113,7 @@ export default function AndroidSectionOne() {
                                     <a href="https://youtube.com/shorts/6Y1SmeXgj-w?feature=share"
                                         target="_blank" rel="noopener noreferrer">
                                         <button className="  transform rounded-2xl  text-neutral-300 bg-neutral-700   px-6 py-2 font-medium  transition-all duration-300 hover:-translate-y-0.5 hover:bg-neutral-400 dark:bg-neutral-600 dark:text-neutral-300 dark:hover:bg-neutral-500">
-                                            Demo Video
+                                           Watch Demo Video
                                         </button>
                                     </a>
                                 </div>
