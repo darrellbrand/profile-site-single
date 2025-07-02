20a0b1ac4c9eb49e2500d2b2d92c51725e97f4d8
dj <darrellbrand@gmail.com>
2025-07-02 13:24:02 -0700

    escape apostrophe

--- src/components/profile-section.tsx
+++ src/components/profile-section.tsx
@@ -29,7 +29,7 @@ export default function ProfileSectionOne() {
                                     <CardDescription className="text-xl text-center  md:text-xl lg:text-2xl dark:text-neutral-300  text-neutral-500">
                                         With 8 years of experience, I build high-performance Android and full-stack web applications. At ESChat, I worked with Kotlin, Java, Jetpack Compose, MVVM, and Dagger Hilt, ensuring quality with Appium tests.
 
-                                        On the web, I create responsive Next.js frontends connected to secure Spring Boot backends. I'm fluent in languages like Python, Lua, Bash, C/C++, and NSIS, bringing strong engineering fundamentals and a focus on clean, maintainable code to every project.
+                                        On the web, I create responsive Next.js frontends connected to secure Spring Boot backends. I&apos;m fluent in languages like Python, Lua, Bash, C/C++, and NSIS, bringing strong engineering fundamentals and a focus on clean, maintainable code to every project.
 
 
                                     </CardDescription>
